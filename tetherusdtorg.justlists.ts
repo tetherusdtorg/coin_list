9f7075e8a3b05e438ca58a30f449ebec4c2035fb
Tether Usdt <info@tetherusdt.org>
2025-02-25 00:25:57 +0300

    Add files via upload

---
+++ tetherusdtorg.justlists.ts
@@ -0,0 +1,22 @@
+{
+    "name":"Coin Swap List",
+    "tokens":[
+        
+        {
+            "symbol":"USDT ",
+            "address":"TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t",
+            "chainId":1,
+            "decimals":6,
+            "name":"USD Tether ",
+            "logoURI":"https://coin.top/production/logo/usdtlogo.png"
+        }
+        
+    ],
+    "logoURI":"https://justswap.io/favicon.ico",
+    "version":{
+        "patch":1,
+        "major":1,
+        "minor":0
+    },
+    "timestamp":1611147166000
+}
